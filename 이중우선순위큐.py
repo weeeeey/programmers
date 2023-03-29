b658e6de5d4f741215ddbeaf4e133c2c6b722b3f
weeeeey <qser155@naver.com>
2023-03-29 20:52:56 +0900

    #

---
+++ 이중우선순위큐.py
@@ -0,0 +1,29 @@
+import heapq
+
+def solution(operations):
+    answer = []
+    q=[]
+    for op in operations:
+        a,b = op.split(" ")
+        if(a=="I"):
+            heapq.heappush(q,int(b))
+        elif not q:
+            continue
+        elif(b=="-1"):
+            heapq.heappop(q)
+        else:
+            q.pop()
+        print(q)
+    if not q:
+        answer=[0,0]
+    else:
+        answer=[max(q),q[0]]
+    return answer
+
+if __name__=='__main__':
+    t=[]
+    for i in range(9):
+        t.append(input().rstrip()) 
+    solution(t)
+
+    
