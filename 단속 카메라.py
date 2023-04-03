b3d2e3235ccbdca16956d3ed5322211ef857de2d
weeeeey <qser155@naver.com>
2023-04-03 20:17:45 +0900

    # 04.03

--- 단속 카메라.py
+++ 단속 카메라.py
@@ -1,12 +1,17 @@
+
 def solution(routes):
     n = len(routes) #자동차 갯수
     answer = 1
-    routes.sort(key=lambda x:(x[0],-x[1]))
-    camera = [routes[0]]
+    routes.sort(key=lambda x:x[1])
+    camera = routes[0][1]
+    for i in range(1,n):
+        s,e = routes[i]
+        if camera<s:
+            camera=e
+            answer+=1
+        else:
+            continue
     
-    for route in routes:
-        s,e = route
-        
     return answer
 
 print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]]))
--- 야근 지수.py
+++ 야근 지수.py
@@ -1,10 +1,20 @@
+import heapq
+
 def solution(n, works):
+    if(sum(works)<=n):
+        return 0
     answer = 0
-    
+    q = []
+    for i in range(len(works)): 
+        heapq.heappush(q,(-works[i]))
+    while(n!=0):
+        max_n = heapq.heappop(q)*(-1)
+        max_n-=1
+        n-=1
+        heapq.heappush(q,(-max_n))
+    for i in range(len(q)):
+        answer+=q[i]**2
     return answer
 
-if __name__=='__main__':
-    work = list(map(int,input().rsplit()))
-    n = int(input().rstrip())
     
-    print(solution(n,work))
+print(solution(4,[4,3,3]))
