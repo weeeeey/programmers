d522652c30b9370312dfd2125d52a5612048df15
weeeeey <qser155@naver.com>
2023-04-10 17:50:13 +0900

    #4.10 (2)

--- 가장 먼 노드.py
+++ 가장 먼 노드.py
@@ -1,5 +1,26 @@
+import heapq
+INF = int(1e9)
+
 def solution(n, edge):
     answer = 0
-    return answer
+    gr = [[]for i in range(n+1)]
+    dit = [INF]*(n+1)
+    dit[1]=0
+    q = []
+    for s,e in edge:
+        gr[s].append(e)
+        gr[e].append(s)
+    heapq.heappush(q,(0,1))
+    m=0
+    while(q):
+        dd, cur = heapq.heappop(q)
+        for next in gr[cur]:
+            d = dd+1
+            if dit[next]>d:
+                dit[next]=d
+                heapq.heappush(q,(d,next))
+                m=max(m,d)
+
+    return dit.count(m)
 
 print(solution(6,[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
