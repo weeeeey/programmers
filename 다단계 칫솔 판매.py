1372a874cf6fb9ad5c73d99a6150cdb3476020f3
weeeeey <qser155@naver.com>
2023-04-26 20:10:56 +0900

    #4.26

--- 다단계 칫솔 판매.py
+++ 다단계 칫솔 판매.py
@@ -5,29 +5,37 @@ def solution(   en,     ref,    sel,            amount):
     n = len(en)
     answer = [0]*n
     tree = dict()
-    empty = []
     profit = dict()
+    profit["center"] = 0
+    tree["center"]="center"
     for i in range(n):
         profit[en[i]]=0
         if ref[i]=='-':
-            continue
-        tree[en[i]]=ref[i]
-    
-
+            tree[en[i]]='center'
+        else:
+            tree[en[i]]=ref[i]
     for i in range(len(sel)):
         name = sel[i]
-        a = amount[i]*100
-        sell,ps = a*0.9, a*0.1
-        profit[name]+=int(sell)
-        p = name
-        while(p in tree):
-            profit[tree[p]]+=int(ps)
-            break    
-    print(profit)
-    # return answer
+        parent = tree[name]
+        money = amount[i]*100
+        profit[name]+=int(money*0.9)
+        profit[parent]+=int(money*0.1)
+        money = int(money*0.01)
+        while(True):
+            profit[parent]-=money
+            parent=tree[parent]
+            profit[parent]+=money
+            money=int(money*0.1)
+            if(money<0 or parent=="center"):
+                break
+    for i in range(n):
+        answer[i]=profit[en[i]]
+    return answer
 
 print(solution(["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"],
                 ["-",     "-",   "mary", "edward", "mary", "mary", "jaimie", "edward"],
                 ["young", "john", "tod", "emily", "mary"],
                 [12, 4, 2, 5, 10]))
 
+["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
+[360,     958,      108,     0,     450,     18,      180,   1080]
