387307e04bf50fcd08d22a7f9479485bf24e6757
weeeeey <qser155@naver.com>
2023-05-08 20:55:43 +0900

    # 5.08

--- 가장 긴 팰린드롬.py
+++ 가장 긴 팰린드롬.py
@@ -1,33 +1,29 @@
 
-def solution(s):
-    e = len(s)
-    def find(s,start,end,ans):
-        if(start<0 or end>e):
-            return 1
-        if (start>end):
-            return ans
-        if(end-start<ans):
-            return ans
-        mid = (start+end)//2
+def solution(se):
+    answer = 0
+    n = len(se)
+    for mid in range(1,n):
         left,right = mid-1,mid+1
-        result = 1
-        while(left>=0 and right<e):
-            if s[left]==s[right]:
-                result+=2
+        ord = 1
+        while(left>=0 and right<n):
+            if se[left]==se[right]:
                 left-=1
-                right+=1 
+                right+=1
+                ord+=2
             else:
                 break
-        result = max(result,ans)
-        a = find(s,start,mid-1,result)
-        b = find(s,mid+1,end,result)
-        c = max(a,b)
-        return max(result,c)
-    
-    answer = find(s,0,e,1)
-    
+        rrd=0
 
-        
+        left,right = mid,mid+1
+        while(left>=0 and right<n):
+            if se[left]==se[right]:
+                left-=1
+                right+=1
+                rrd+=2
+            else:
+                break
+        answer= max(answer,max(ord,rrd))
+                
     return answer
 
-print(solution("abacde"))
+print(solution("abaccaba"))
