46fdbad0a48ab1d041b7b77e6ac3ef88391e355d
zerojin <qser155@naver.com>
2023-09-16 19:23:48 +0900

    4n타일링 dp

--- 11333.py
+++ 11333.py
@@ -1,14 +1,21 @@
-# def solution(n):
-#     answer = 0
-#     dp = [0]*n
+def solution(n):
+    answer = 0
+    if (n % 3 != 0):
+        return 0
+    dp = [0]*(n+1)
+    dp[1] = 2
+    dp[2] = 2
+    dp[3] = 3
+    for i in range(4, n+1):
+        if (i % 3 == 1):
+            dp[i] = dp[i-1]*2 + dp[i-3]
+        else:
+            dp[i] = dp[i-1]+dp[i-3]
 
-#     return answer
+    return dp[n] % 1000000007
 
 
-# T = int(input())
-# for i in range(T):
-#     N = int(input())
-#     print(solution(N))
-
-INF = int(1000000007)
-print((3*2**1111+1) % INF - 524313417)
+T = int(input())
+for i in range(T):
+    N = int(input())
+    print(solution(N))
---
+++ 전력망을 둘로 나누기.py
@@ -0,0 +1,14 @@
+def solution(n, wires):
+    answer = -1
+    return answer
+
+
+print(solution(9,
+               [[1, 3],
+                [2, 3],
+                   [3, 4],
+                   [4, 5],
+                   [4, 6],
+                   [4, 7],
+                   [7, 8],
+                   [7, 9]]))
