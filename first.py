2047290411f242613b563660bdb26d373d74904d
zerojin <qser155@naver.com>
2023-09-18 01:46:45 +0900

    #

--- first.py
+++ first.py
@@ -1,6 +1,25 @@
-def solution(total_sp, skills):
-    answer = []
+def binary(n):
+    cnt = 0
+    while (n != 0):
+        n &= (n-1)
+        cnt += 1
+    return cnt
+
+
+def solution(n):
+    if (n == 1):
+        return 0
+    answer = 0
+    m = binary(n)
+    print(m)
+    start = 0
+    for i in range(m):
+        start += 2**i
+    for i in range(start, n):
+        if (binary(i) == m):
+            answer += 1
+
     return answer
 
 
-print(solution(121, [[1, 2], [1, 3], [3, 6], [3, 4], [3, 5]]))
+print(solution(11))
