1baf65546aa82280ca1bcf3b7a2cd7939d9a48a0
zerojin <qser155@naver.com>
2023-09-18 13:18:47 +0900

    연속 부분 수열 합의 개수

--- 연속 부분 수열 합의 개수.py
+++ 연속 부분 수열 합의 개수.py
@@ -1,6 +1,17 @@
 def solution(elements):
     answer = 0
-    return answer
+    n = len(elements)
+    arr = elements+elements
+    dp = [[0]*(2*n) for i in range(n+1)]
+    result = []
+    for i in range(1, n+1):
+        for j in range(n):
+            dp[i][j] = dp[i-1][j]+arr[j+i-1]
+            result.append(dp[i][j])
 
+    return len(set(result))
+
+
+# 3<=길이<=1_000
 
 print(solution([7, 9, 1, 1, 4]))
