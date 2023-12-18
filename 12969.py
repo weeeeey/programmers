d6755a4a3bf7611da7d77762c42b70f7e9ffc3cf
zerojin <qser155@naver.com>
2023-12-18 15:27:51 +0900

    12969.py

--- 12969.py
+++ 12969.py
@@ -1,4 +1,40 @@
 from collections import deque
 
+
+def sol(n, k):
+    if not k:
+        return "A"*n
+    dx = ["A", "B", "C"]
+    q = deque()
+    q.append((0, 0, 0, 0, 0, []))
+    visited = [[[[False]*(451)for _ in range(31)] for _ in range(31)]
+               for _ in range(31)]
+    visited[0][0][0][0] = True
+    while (q):
+        sum_k, a, b, c, cur, arr = q.popleft()
+
+        if (sum_k > k):
+            continue
+        if (cur == n):
+            if (sum_k == k):
+                return "".join(arr)
+            else:
+                continue
+        for i in range(3):
+            nx = dx[i]
+            if (i == 0 and visited[a+1][b][c][sum_k] == False):
+                visited[a+1][b][c][sum_k] = True
+                q.append((sum_k, a+1, b, c, cur+1, arr+[nx]))
+
+            if (i == 1 and visited[a][b+1][c][sum_k+a] == False):
+                visited[a][b+1][c][sum_k+a] = True
+                q.append((sum_k+a, a, b+1, c, cur+1, arr+[nx]))
+
+            if (i == 2 and visited[a][b][c+1][sum_k+a+b] == False):
+                visited[a][b][c+1][sum_k+a+b] = True
+                q.append((sum_k+a+b, a, b, c+1, cur+1, arr+[nx]))
+    return -1
+
+
 n, k = map(int, input().rsplit())
-q = deque()
+print(sol(n, k))
